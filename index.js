43d828773ca28e3b0a055cc63b79712d71f26a6b
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-05-15 13:50:06 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const discord = require("discord.js");
-const client = new discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 client.on("guildMemberAdd", member => {
   member.setNickname("Noobie");
